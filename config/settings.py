591c177f10057c89bc09ca5732a00f81feca4d2d
GFmatrix <sketchapps.lp@gmail.com>
2024-09-14 18:52:51 +0500

    Static Files config add

--- config/settings.py
+++ config/settings.py
@@ -144,6 +144,10 @@ LOCALE_PATHS = [
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('web.urls')),
@@ -8,3 +10,4 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
 ]
 urlpatterns += i18n_patterns(path("admin/", admin.site.urls))
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
