ea49489d76eca3d6fe08bc5727ba30f315210a09
Shermatov Davlatbek <sh.davlatbek006@gmail.com>
2024-10-09 16:20:48 +0500

    Change template dir, add All News view

--- web/views.py
+++ web/views.py
@@ -1,18 +1,26 @@
+import os
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from .models import News, Medicine
 
+PAGES_DIR = "pages"
+
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home.html', {'news': News.objects.all()})
+        return render(request,  os.path.join(PAGES_DIR, "home.html"), {'news': News.objects.all()})
+    
+    
+class NewsAll(View):
+    def get(self, request, slug):
+        return render(request, os.path.join(PAGES_DIR, "news.html"), {'news': News.objects.all()})
     
     
 class NewsDetail(View):
     def get(self, request, slug):
-        return render(request, 'coming-soon.html', {'news': News.objects.get(slug=slug)})
-    
+        return render(request, os.path.join(PAGES_DIR, "news-detail.html"), {'news': News.objects.get(slug=slug)})
+
 
 class ComingSoon(View):
     def get(self, request):
