49615b56d9059c711fc74a17faf3edcef3c5d0b9
Shermatov Davlatbek <sh.davlatbek006@gmail.com>
2024-10-09 16:19:36 +0500

    Rename news-detail add news-all

--- web/urls.py
+++ web/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
-from .views import HomeView, NewsDetail, ComingSoon
+from .views import HomeView, NewsAll, NewsDetail, ComingSoon
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('news/<slug:slug>/', NewsDetail.as_view(), name="news_detail"),
+    path('news/', NewsAll.as_view(), name="news-all"),
+    path('news/<slug:slug>/', NewsDetail.as_view(), name="news-detail"),
     path("coming-soon/", ComingSoon.as_view(), name="coming-soon")
 ]
