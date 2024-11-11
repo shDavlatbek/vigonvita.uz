0d75c86a52563ee9b66723ea1ec5eca0e7f3bbc2
shDavlatbek <sh.davlatbek006@gmail.com>
2024-11-11 14:27:47 +0500

    Change name staticfiles folder to public

--- public/assets/js/aivons.js
+++ public/assets/js/aivons.js
@@ -457,33 +457,33 @@
   });
 
   if ($(".tabs-box").length) {
-    $(".tabs-box .tab-buttons .tab-btn").on("click", function (e) {
-      e.preventDefault();
-      var target = $($(this).attr("data-tab"));
+    // $(".tabs-box .tab-buttons .tab-btn").on("click", function (e) {
+    //   e.preventDefault();
+    //   var target = $($(this).attr("data-tab"));
 
-      if ($(target).is(":visible")) {
-        return false;
-      } else {
-        target
-          .parents(".tabs-box")
-          .find(".tab-buttons")
-          .find(".tab-btn")
-          .removeClass("active-btn");
-        $(this).addClass("active-btn");
-        target
-          .parents(".tabs-box")
-          .find(".tabs-content")
-          .find(".tab")
-          .fadeOut(0);
-        target
-          .parents(".tabs-box")
-          .find(".tabs-content")
-          .find(".tab")
-          .removeClass("active-tab");
-        $(target).fadeIn(300);
-        $(target).addClass("active-tab");
-      }
-    });
+    //   if ($(target).is(":visible")) {
+    //     return false;
+    //   } else {
+    //     target
+    //       .parents(".tabs-box")
+    //       .find(".tab-buttons")
+    //       .find(".tab-btn")
+    //       .removeClass("active-btn");
+    //     $(this).addClass("active-btn");
+    //     target
+    //       .parents(".tabs-box")
+    //       .find(".tabs-content")
+    //       .find(".tab")
+    //       .fadeOut(0);
+    //     target
+    //       .parents(".tabs-box")
+    //       .find(".tabs-content")
+    //       .find(".tab")
+    //       .removeClass("active-tab");
+    //     $(target).fadeIn(300);
+    //     $(target).addClass("active-tab");
+    //   }
+    // });
   }
 
 
