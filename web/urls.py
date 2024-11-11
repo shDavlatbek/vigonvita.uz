a15ae0b635b3855758efdd0c19a1461489224fef
Shermatov Davlatbek <sh.davlatbek006@gmail.com>
2024-11-12 03:29:40 +0500

    Medicine detail

--- web/urls.py
+++ web/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
-from .views import HomeView, NewsAll, NewsDetail, ComingSoon
+from .views import HomeView, NewsAll, NewsDetail, MedicineDetail, ComingSoon
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('news/', NewsAll.as_view(), name="news-all"),
     path('news/<slug:slug>/', NewsDetail.as_view(), name="news-detail"),
+    path('medicine/<int:pk>/', MedicineDetail.as_view(), name="medicine-detail"),
     path("coming-soon/", ComingSoon.as_view(), name="coming-soon")
 ]
