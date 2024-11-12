27f2a557ee70f1acacd6b43bf9fbdb2a41c7a8c5
Shermatov Davlatbek <sh.davlatbek006@gmail.com>
2024-11-12 16:52:36 +0500

    Fix bug with rich text V2

--- web/templatetags/custom_tags.py
+++ web/templatetags/custom_tags.py
@@ -7,3 +7,8 @@ def short_description(value, max_length=100):
     if len(value) > max_length:
         return value[:max_length] + "..."
     return value
+
+
+@register.filter
+def replace_char(value, replace_char="&nbsp;", with_char=" "):
+    return str(value).replace(replace_char, with_char)
