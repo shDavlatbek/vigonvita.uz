bbce65f22484786cef25a39b266dc4545c0946a9
Shermatov Davlatbek <sh.davlatbek006@gmail.com>
2024-12-10 05:25:52 +0500

    Big changes

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -3,4 +3,23 @@
     Fancybox.bind('[data-fancybox]', {
         // Your custom options
     });    
+    var swiper_photo = new Swiper("#swiper-photos", {
+        slidesPerView: 'auto',
+        spaceBetween: 40,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+          },
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        loop: true,
+        coverflowEffect: {
+            rotate: 0,
+            stretch: 0,
+            depth: 120,
+            modifier: 1,
+            slideShadows: false,
+        }
+    });
 })(jQuery);
